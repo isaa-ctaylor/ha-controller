9886c20ec3f7854cda87e8c9c6a720dbf50faa83
isaa-ctaylor <isaac@isaactaylor.xyz>
2024-03-27 17:41:29 +0000

    Add menu in index; move lights control to lights.view

--- app/index.js
+++ app/index.js
@@ -1,83 +1,94 @@
 import * as document from "document";
 import { peerSocket } from "messaging";
 import { vibration } from "haptics";
-
-let lightsList = document.getElementById("lightsList");
-let loading = document.getElementById("loading");
-let error = document.getElementById("error");
-
-let NUM_ELEMS = 0;
-
+import { LongPressDetector } from "fitbit-gestures";
 
 peerSocket.addEventListener("open", (evt) => {
-    peerSocket.send({ "command": "fetch", "data": null });
-});
+    console.log("Socket open");
+    document.getElementById("light-button").onclick = function (evt) {
+        document.location.assign("views/lights.view");
+
+        let lights = [];
+        let tiles = [];
+
+        let NUM_ELEMS = 0;
+
+        peerSocket.addEventListener("message", (evt) => {
+            if (evt.data.command == "fetch") {
+                let lightsList = document.getElementById("lightsList");
+                let loading = document.getElementById("loading");
+                lights = evt.data.data;
+                NUM_ELEMS = lights.length;
+                loading.style.display = "none";
+                lightsList.style.display = "inline";
+                lightsList.delegate = {
+                    getTileInfo: (index) => {
+                        return {
+                            type: "my-pool",
+                            value: "Item",
+                            index: index,
+                            entity_id: lights[index][0],
+                            friendly_name: lights[index][1],
+                            state: lights[index][2],
+                            supported_colour_modes: lights[index][3],
+                            colour: lights[index][3],
+                        };
+                    },
+                    configureTile: (tile, info) => {
+                        if (info.type == "my-pool") {
+                            tile.getElementById("name").text = `${info.friendly_name}`;
+                            tiles.push(tile);
+                            let button = tile.getElementById("toggle-button");
+                            if (info.state == "on") {
+                                let img = button.getElementById("image");
+                                img.style.fill = "#ffc107";
+
+                                let bg = button.getElementsByTagName("circle")[0];
+                                bg.style.fill = "#ffc107";
+                            }
+                            if (JSON.stringify(info.supported_colour_modes) != JSON.stringify(["onoff"])) {
+                                let detector = new LongPressDetector(button, (e) => {
+                                    console.log("Long press!");
+                                })
+                            }
+                            tile.data_entity_id = info.entity_id;
+                            button.onclick = function (evt) {
+                                vibration.start("confirmation");
+                                peerSocket.send({ "command": "toggle", "data": { "entity_id": info.entity_id } })
+                            };
+                        }
+                    }
+                };
+
+                // length must be set AFTER delegate
+                lightsList.length = NUM_ELEMS;
+            } else if (evt.data.command == "update") {
+                lights.every((item, index) => {
+                    if (item[0] == evt.data.data.entity_id) {
+                        let tile = tiles[index];
+                        let button = tile.getElementById("toggle-button");
+                        let img = button.getElementById("image");
+                        img.style.fill = (evt.data.data.state == "on") ? "#ffc107" : "#9e9e9e";
+                        // img.href = (evt.data.data.state == "on") ? "lightbulb-on-g.png" : "lightbulb-off-g.png";
+                        let bg = button.getElementsByTagName("circle")[0];
+                        bg.style.fill = (evt.data.data.state == "on") ? "#ffc107" : "#9e9e9e";
+                        return false;
+                    }
+                    return true;
+                })
+            } else if (evt.data.command == "error") {
+                let error = document.getElementById("error");
+
+                document.getElementById("error-message").text = evt.data.data;
+                error.style.display = "inline";
+            };
+        });
+
+        peerSocket.send({ "command": "fetch", "data": null });
+    }
+})
 
 peerSocket.addEventListener("error", (err) => {
     console.error(`Connection error: ${err.code} - ${err.message}`);
 });
 
-let lights = [];
-let tiles = [];
-
-peerSocket.addEventListener("message", (evt) => {
-    if (evt.data.command == "fetch") {
-        lights = evt.data.data;
-        NUM_ELEMS = lights.length;
-        loading.style.display = "none";
-        lightsList.style.display = "inline";
-        lightsList.delegate = {
-            getTileInfo: (index) => {
-                return {
-                    type: "my-pool",
-                    value: "Item",
-                    index: index,
-                    entity_id: lights[index][0],
-                    friendly_name: lights[index][1],
-                    state: lights[index][2],
-                    supported_colour_modes: lights[index][3],
-                    colour: lights[index][3],
-                };
-            },
-            configureTile: (tile, info) => {
-                if (info.type == "my-pool") {
-                    tile.getElementById("name").text = `${info.friendly_name}`;
-                    tiles.push(tile);
-                    let button = tile.getElementById("toggle-button");
-                    if (info.state == "on") {
-                        let img = button.getElementById("image");
-                        img.style.fill = "#ffc107";
-
-                        let bg = button.getElementsByTagName("circle")[0];
-                        bg.style.fill = "#ffc107";
-                    }
-                    tile.data_entity_id = info.entity_id;
-                    button.onclick = function (evt) {
-                        vibration.start("confirmation");
-                        peerSocket.send({ "command": "toggle", "data": { "entity_id": info.entity_id } })
-                    };
-                }
-            }
-        };
-
-        // length must be set AFTER delegate
-        lightsList.length = NUM_ELEMS;
-    } else if (evt.data.command == "update") {
-        lights.every((item, index) => {
-            if (item[0] == evt.data.data.entity_id) {
-                let tile = tiles[index];
-                let button = tile.getElementById("toggle-button");
-                let img = button.getElementById("image");
-                img.style.fill = (evt.data.data.state == "on") ? "#ffc107" : "#9e9e9e";
-                // img.href = (evt.data.data.state == "on") ? "lightbulb-on-g.png" : "lightbulb-off-g.png";
-                let bg = button.getElementsByTagName("circle")[0];
-                bg.style.fill = (evt.data.data.state == "on") ? "#ffc107" : "#9e9e9e";
-                return false;
-            }
-            return true;
-        })
-    } else if (evt.data.command == "error") {
-        document.getElementById("error-message").text = evt.data.data;
-        error.style.display = "inline";
-    };
-});
